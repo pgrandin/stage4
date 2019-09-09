b5adc075888b4ff1d79432f47ca7680490942704
Pierre Grandin <grandinp@gmail.com>
2019-09-09 13:26:57 -0600

    Improving build script

--- step2.sh
+++ step2.sh
@@ -22,21 +22,15 @@ echo "=sys-kernel/gentoo-sources-$kversion ~amd64" > /etc/portage/package.keywor
 FEATURES="-getbinpkg" emerge -q =gentoo-sources-$kversion
 
 cd /usr/src/linux
-cat arch/x86/configs/x86_64_defconfig /tmp/kernel-configs-master/*_defconfig > arch/x86/configs/${branch}_defconfig
+cat arch/x86/configs/x86_64_defconfig /tmp/kernel-configs-master/docker_defconfig /tmp/kernel-configs-master/${branch}_defconfig > arch/x86/configs/${branch}_defconfig
 make defconfig ${branch}_defconfig
 make -j8
 make modules_install
 cp arch/x86_64/boot/bzImage /boot/linux-${kversion}-gentoo
 
-emerge -q1 dev-lang/perl
-FEATURES="-getbinpkg" emerge -q1 XML-Parser
-perl-cleaner --all
-emerge -NDuq @base
 FEATURES="-sandbox -usersandbox" emerge -eq @world
 
 make mrproper
-
-curl -L https://github.com/prusa3d/Slic3r/releases/download/version_1.41.3/Slic3rPE-1.41.3+linux64-full-201902121303.AppImage --output /usr/local/bin/Slic3rPE-1.41.3+linux64-full-201902121303.AppImage
 
 cp /usr/share/zoneinfo/America/Denver /etc/localtime
 echo "America/Denver" > /etc/timezone
@@ -44,18 +38,13 @@ echo "America/Denver" > /etc/timezone
 echo "root:scrambled" | chpasswd
 
 pushd /etc/init.d
-ln -s net.lo net.enp62s0u1u2
-ln -s net.lo net.wlp2s0
-rc-update add net.enp62s0u1u2 default
+ln -s net.lo net.eno1
+rc-update add net.eno1 default
 rc-update add sshd default
 popd
 
 
-sed -i -e 's/localhost/dell/' /etc/conf.d/hostname
-sed -i -e 's/"xdm"/"slim"/' /etc/conf.d/xdm
-sed -i -e 's/#default_user        simone/default_user        pierre/g' /etc/slim.conf
-sed -i -e 's/current_theme       default/current_theme       slim-gentoo-simple/g' /etc/slim.conf
-sed -i -e 's/#focus_password      no/focus_password      yes/g' /etc/slim.conf
+sed -i -e "s/localhost/${branch}/" /etc/conf.d/hostname
 
 useradd pierre
 usermod -aG wheel,uucp,audio,video,usb,docker,kvm pierre
