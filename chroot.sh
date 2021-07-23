dd25855feff0fea45e60f9e76aa72414c16fc2df
Pierre Grandin <grandinp@gmail.com>
2021-07-23 10:33:21 -0600

    Cleanup chroot before packing the stage4

--- chroot.sh
+++ chroot.sh
@@ -6,6 +6,10 @@ chroot /mnt/gentoo/ /bin/bash /step2.sh Z390
 umount -l ${stage4_fs}/dev
 umount -l ${stage4_fs}/proc
 
+rm -rf ${stage4_fs}/var/cache && mkdir ${stage4_fs}/var/cache
+rm -rf ${stage4_fs}/var/tmp && mkdir ${stage4_fs}/var/tmp
+rm -rf ${stage4_fs}/use/portage && mkdir ${stage4_fs}/usr/portage
+
 pushd ${stage4_fs}
 tar cfz /stage4.tgz .
 popd
