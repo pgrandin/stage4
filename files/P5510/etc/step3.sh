0db760cb5be66b3779e7a85d94dbd5a93e8d7962
Pierre Grandin <grandinp@gmail.com>
2021-10-25 16:01:27 -0600

    Deprecated target P5510

--- files/P5510/etc/step3.sh
+++
@@ -1,6 +0,0 @@
-sed -i -e 's/"xdm"/"slim"/' /etc/conf.d/xdm
-sed -i -e 's/#default_user        simone/default_user        pierre/g' /etc/slim.conf
-sed -i -e 's/current_theme       default/current_theme       slim-gentoo-simple/g' /etc/slim.conf
-sed -i -e 's/#focus_password      no/focus_password      yes/g' /etc/slim.conf
-
-rc-update add xdm default
