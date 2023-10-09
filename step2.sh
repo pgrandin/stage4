47d938e8130ce97edc7864fbdd2b6cbf62e50c6b
Pierre Grandin <grandinp@gmail.com>
2023-10-09 15:48:26 +0000

    Minor improvements

--- step2.sh
+++ step2.sh
@@ -1,6 +1,6 @@
 set -e
 
-branch=$1
+target=$1
 
 env-update && source /etc/profile
 emerge-webrsync
@@ -8,19 +8,28 @@ emerge-webrsync
 eselect profile set default/linux/amd64/17.1
 
 MAKEOPTS="-j$(nproc)" emerge -q eix app-misc/jq
+[ -d /var/cache/eix ] || mkdir /var/cache/eix
+chown portage:portage /var/cache/eix
+
 eix-update
 
-kversion=$(eix gentoo-source|awk -F'[()]' '/ [~]?5.10/ {version=$2} END{print version}')
-echo "=sys-kernel/gentoo-sources-$kversion ~amd64" > /etc/portage/package.keywords/gentoo-sources
+kversion=$(eix gentoo-source|awk -F'[()]' '/ [~]?6.1./ {version=$2} END{print version}')
+echo "=sys-kernel/gentoo-sources-$kversion ~amd64" > /etc/portage/package.accept_keywords/gentoo-sources
 MAKEOPTS="-j$(nproc)" FEATURES="-getbinpkg" emerge -q =gentoo-sources-$kversion
-
-wget http://packages.kazer.org:8080/Z390/kernel-${kversion}.tgz -O /tmp/kernel.tgz
-tar xvfz /tmp/kernel.tgz -C /
 pushd /usr/src
 ln -s linux-${kversion}-gentoo linux
 popd
 
-echo "efibootmgr -c -d /dev/sda -p 1 -l 'linux-${kversion}-gentoo' -L 'Gentoo-${kversion}'" > /root/setup_efi.sh
+# Extract PORTAGE_BINHOST value from make.conf
+portage_binhost_value=$(grep 'PORTAGE_BINHOST=' /etc/portage/make.conf | cut -d'"' -f2)
+# Infer kernel url
+kernel_url="${portage_binhost_value%binpkgs/}kernel-${kversion}.tgz"
+echo "$kernel_url"
+
+wget ${kernel_url} -O /tmp/kernel-${kversion}.tgz
+tar xvfz /tmp/kernel-${kversion}.tgz -C /
+
+echo "efibootmgr -c -d /dev/nvme0n1 -l '\EFI\gentoo-${kversion}' -L 'Gentoo-${kversion}'" > /root/setup_efi.sh
 
 MAKEOPTS="-j$(nproc)" emerge -eq @world --jobs 4
 
@@ -29,7 +38,7 @@ echo "America/Denver" > /etc/timezone
 
 echo "root:scrambled" | chpasswd
 
-netif=$(cat /config.json | jq --arg HOST $branch -r '.configs[] | select (.["host"]==$HOST) | .network_interface')
+netif=$(cat /config.json | jq --arg HOST $target -r '.configs[] | select (.["host"]==$HOST) | .network_interface')
 
 pushd /etc/init.d
 ln -s net.lo net.${netif}
@@ -39,7 +48,7 @@ rc-update add syslog-ng default
 popd
 
 
-sed -i -e "s/localhost/${branch}/" /etc/conf.d/hostname
+sed -i -e "s/localhost/${target}/" /etc/conf.d/hostname
 
 [ -f /etc/step3.sh ] && /bin/bash /etc/step3.sh
 
