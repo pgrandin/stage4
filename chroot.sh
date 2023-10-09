fd035ef6fa33f265908361e27105923bbeef6a79
Pierre Grandin <grandinp@gmail.com>
2023-10-09 15:49:04 +0000

    leverage tmpfs more

--- chroot.sh
+++ chroot.sh
@@ -4,14 +4,14 @@ echo "Building for target ${target}"
 
 mount -t proc /proc ${stage4_fs}/proc
 mount --rbind /dev ${stage4_fs}/dev
+mount -t tmpfs tmpfs ${stage4_fs}/var/tmp
+mount -t tmpfs tmpfs ${stage4_fs}/var/cache
 
 chroot /mnt/gentoo/ /bin/bash /step2.sh $target
 
 umount -l ${stage4_fs}/dev
 umount -l ${stage4_fs}/proc
 
-rm -rf ${stage4_fs}/var/cache && mkdir ${stage4_fs}/var/cache
-rm -rf ${stage4_fs}/var/tmp && mkdir ${stage4_fs}/var/tmp
 rm -rf ${stage4_fs}/use/portage && mkdir ${stage4_fs}/usr/portage
 
 pushd ${stage4_fs}
