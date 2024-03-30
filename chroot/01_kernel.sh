0ef298b22ff5d205564784c76c9c261f2663968f
Pierre Grandin <grandinp@gmail.com>
2024-03-29 18:23:05 -0600

    working on gh-actions

---
+++ chroot/01_kernel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+target=$1
+[[ -z "${target}" ]] && exit 1
+
+[[ -d /tmp/kernel-configs-master ]] && rm -rf /tmp/kernel-configs-master
+# Prepare kernel config
+git clone https://github.com/pgrandin/kernel-configs.git /tmp/kernel-configs-master/
+export kconfig_sha=$(cd /tmp/kernel-configs-master/ && git rev-parse HEAD)
+export kversion=$(eix gentoo-source|awk -F'[()]' '/ [~]?6.1./ {version=$2} END{print version}')
+cat /tmp/kernel-configs-master/common_defconfig /tmp/kernel-configs-master/${target}_defconfig > /${target}_defconfig
+
+confs=$(yq -r '.kernel_fragments[]' /config.yml)
+for conf in $confs; do
+    echo "# for ${conf}" >> /${target}_defconfig
+    cat /tmp/kernel-configs-master/${conf}_defconfig >> /${target}_defconfig
+done
+
+echo "=sys-kernel/gentoo-sources-${kversion} ~amd64" > /etc/portage/package.accept_keywords/gentoo-sources
+FEATURES="-getbinpkg" emerge -j$(nproc) -q =gentoo-sources-${kversion} sys-kernel/linux-firmware
+cd /usr/src && ln -sf linux-${kversion}-gentoo linux
+export kpath="linux"
+cat /usr/src/${kpath}/arch/x86/configs/x86_64_defconfig /${target}_defconfig  > /usr/src/${kpath}/arch/x86/configs/${target}_defconfig
+cd /usr/src/${kpath} && make defconfig ${target}_defconfig
+
+cd /usr/src/${kpath} && make -j$(nproc) && make modules_install
+
+tar cvfz /kernel-${kversion}.tgz /lib/modules/${kversion}-gentoo /usr/src/${kpath}/arch/x86_64/boot/bzImage
