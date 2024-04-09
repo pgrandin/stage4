fb72296ce24eaa66f9de75f9d01e3d0b2e1b481d
Pierre Grandin <grandinp@gmail.com>
2024-04-09 13:20:33 -0600

    Improving content of stage4

--- chroot/01_kernel.sh
+++ chroot/01_kernel.sh
@@ -3,29 +3,29 @@
 set -e
 
 target=$1
-[[ -z "${target}" ]] && exit 1
+[[ -z ${target} ]] && exit 1
 
 [[ -d /tmp/kernel-configs-master ]] && rm -rf /tmp/kernel-configs-master
 # Prepare kernel config
 git clone https://github.com/pgrandin/kernel-configs.git /tmp/kernel-configs-master/
 export kconfig_sha=$(cd /tmp/kernel-configs-master/ && git rev-parse HEAD)
-export kversion=$(eix gentoo-source|awk -F'[()]' '/ [~]?6.1./ {version=$2} END{print version}')
+export kversion=$(eix gentoo-source | awk -F'[()]' '/ [~]?6.1./ {version=$2} END{print version}')
 
-echo "efibootmgr -c -d /dev/nvme0n1 -l '\EFI\gentoo-${kversion}' -L 'Gentoo-${kversion}'" > /root/setup_efi.sh
+echo "efibootmgr -c -d /dev/nvme0n1 -l '\EFI\gentoo-${kversion}' -L 'Gentoo-${kversion}'" >/root/setup_efi.sh
 
-cat /tmp/kernel-configs-master/common_defconfig /tmp/kernel-configs-master/${target}_defconfig > /${target}_defconfig
+cat /tmp/kernel-configs-master/common_defconfig /tmp/kernel-configs-master/${target}_defconfig >/${target}_defconfig
 
 confs=$(yq -r '.kernel_fragments[]' /config.yml)
 for conf in $confs; do
-    echo "# for ${conf}" >> /${target}_defconfig
-    cat /tmp/kernel-configs-master/${conf}_defconfig >> /${target}_defconfig
+	echo "# for ${conf}" >>/${target}_defconfig
+	cat /tmp/kernel-configs-master/${conf}_defconfig >>/${target}_defconfig
 done
 
-echo "=sys-kernel/gentoo-sources-${kversion} ~amd64" > /etc/portage/package.accept_keywords/gentoo-sources
+echo "=sys-kernel/gentoo-sources-${kversion} ~amd64" >/etc/portage/package.accept_keywords/gentoo-sources
 FEATURES="-getbinpkg" emerge -j$(nproc) -q =gentoo-sources-${kversion} sys-kernel/linux-firmware
 cd /usr/src && ln -sf linux-${kversion}-gentoo linux
 export kpath="linux"
-cat /usr/src/${kpath}/arch/x86/configs/x86_64_defconfig /${target}_defconfig  > /usr/src/${kpath}/arch/x86/configs/${target}_defconfig
+cat /usr/src/${kpath}/arch/x86/configs/x86_64_defconfig /${target}_defconfig >/usr/src/${kpath}/arch/x86/configs/${target}_defconfig
 cd /usr/src/${kpath} && make defconfig ${target}_defconfig
 
 cd /usr/src/${kpath} && make -j$(nproc) && make modules_install
--- chroot/04_configure.sh
+++ chroot/04_configure.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+set -e
+
+target=$1
+[[ -z ${target} ]] && exit 1
+
 cp /usr/share/zoneinfo/America/Denver /etc/localtime
 echo "America/Denver" > /etc/timezone
 
@@ -16,6 +23,14 @@ popd
 sed -i -e "s/localhost/${target}/" /etc/conf.d/hostname
 
 [[ -f /etc/conf.d/display-manager ]] && sed -i -e 's/xdm/lightdm/g' /etc/conf.d/display-manager 
+
+[[ -f /etc/libvirt/libvirtd.conf ]] && cat <<EOF > /etc/libvirt/libvirtd.conf
+auth_unix_ro = "none"
+auth_unix_rw = "none"
+unix_sock_group = "libvirt"
+unix_sock_ro_perms = "0777"
+unix_sock_rw_perms = "0770"
+EOF
 
 useradd pierre
 usermod -aG wheel,uucp,audio,video,usb,docker,kvm pierre
