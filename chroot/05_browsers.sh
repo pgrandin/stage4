4ef97ee1badc46469931a42ce3b74fa106c06f27
Pierre Grandin <grandinp@gmail.com>
2024-11-22 10:14:49 -0700

    Fixed yq syntax for 4+

--- chroot/05_browsers.sh
+++ chroot/05_browsers.sh
@@ -5,10 +5,11 @@ set -e
 target=$1
 [[ -z ${target} ]] && exit 1
 
-if yq '.features | any(. == "X")' /config.yml >/dev/null; then
+if yq '.features[] | select(. == "X")' /config.yml >/dev/null 2>&1; then
     echo "X is present in features, building browsers"
     emerge -q www-client/firefox
     emerge -q www-client/chromium
 else
     echo "X is not present in features, not building browsers"
 fi
+
