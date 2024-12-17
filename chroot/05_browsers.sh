48db07f7614c01624b34c4dcd7ce35d9120b866b
Pierre Grandin <grandinp@gmail.com>
2024-12-17 08:23:06 -0700

    Properly setup www-client packages when needed

--- chroot/05_browsers.sh
+++ chroot/05_browsers.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+stage4_fs=$1
+
+if [ ! -f "$stage4_fs/var/lib/portage/world" ]; then
+    echo "The world file does not exist at $stage4_fs/var/lib/portage/world."
+    exit 1
+fi
+
 if yq '.features[] | select(. == "X")' /config.yml | grep -q X; then
     echo "X is present in features, building browsers"
+    echo "setting up firefox package from packages/www-client/firefox to $stage4_fs/etc/portage/"
+    rsync -vrtza packages/www-client/firefox/ ../$stage4_fs/etc/portage/
     emerge -q www-client/firefox
+
+    echo "setting up chromium package from packages/www-client/chromium to $stage4_fs/etc/portage/"
+    rsync -vrtza packages/www-client/chromium/ ../$stage4_fs/etc/portage/
     emerge -q www-client/chromium
 else
     echo "X is not present in features, not building browsers"
