0eb2e247ab51ee6f599b5d3b0ddd39bd9f4a0abe
Pierre Grandin <grandinp@gmail.com>
2024-12-17 16:21:59 -0700

    fix: correct config.yml path in browser build script

--- chroot/05_browsers.sh
+++ chroot/05_browsers.sh
@@ -7,7 +7,7 @@ if [ ! -f "$stage4_fs/var/lib/portage/world" ]; then
     exit 1
 fi
 
-if yq '.features[] | select(. == "X")' /config.yml | grep -q X; then
+if yq '.features[] | select(. == "X")' "$stage4_fs/config.yml" | grep -q X; then
     echo "X is present in features, building browsers"
     echo "setting up firefox package from packages/www-client/firefox to $stage4_fs/etc/portage/"
     rsync -vrtza packages/www-client/firefox/ ../$stage4_fs/etc/portage/
