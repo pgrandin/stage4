856818e4fa80bcd8675f6510026fd2c2485d6531
Pierre Grandin <grandinp@gmail.com>
2024-12-18 11:02:44 -0700

    ensure libvpx is built before firefox, with proper workarounds

--- chroot/05_browsers.sh
+++ chroot/05_browsers.sh
@@ -14,6 +14,7 @@ if yq '.features[] | select(. == "X")' "$stage4_fs/config.yml" | grep -q X; then
     echo "setting up firefox package from packages/www-client/firefox to $stage4_fs/etc/portage/"
     rsync -vrtza packages/www-client/firefox/ $stage4_fs/etc/portage/
     echo "Building firefox from chroot"
+    chroot "${stage4_fs}" env -i HOSTNAME=localhost HOME="$HOME" TERM="$TERM" PATH="$PATH" FEATURES="$FEATURES" emerge -q1 media-libs/libvpx
     chroot ${stage4_fs} emerge -q www-client/firefox
 
     echo "setting up chromium package from packages/www-client/chromium to $stage4_fs/etc/portage/"
